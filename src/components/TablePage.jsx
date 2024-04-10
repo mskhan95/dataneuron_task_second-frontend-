1cc5cae42bdd405f7ce5c9b86cbfb9b6750d9424
mskhan1995 <shabhankhan123456@gmail.com>
2024-04-10 21:18:18 +0530

    Spiiner Added in ui

--- src/components/TablePage.jsx
+++ src/components/TablePage.jsx
@@ -10,6 +10,7 @@ import {
   Button,
   Heading,
   useDisclosure,
+  Spinner,
 } from "@chakra-ui/react";
 import UpdateModal from "./UpdateModal";
 import { updateData } from "../APIRequest/Apirequest";
@@ -34,6 +35,7 @@ const TablePage = ({ alldata, getOneData, getcount, getalldata }) => {
         window.alert("Updated Successfull");
       }
     } catch (error) {
+      window.alert("Some thing went wrong");
       console.error("Error updating data:", error);
     }
     onClose();
@@ -48,39 +50,50 @@ const TablePage = ({ alldata, getOneData, getcount, getalldata }) => {
       <Heading mb={5} textAlign={"center"}>
         Table Page
       </Heading>
-      <div>
-        <TableContainer>
-          <Table variant="simple" size="sm">
-            <Thead>
-              <Tr>
-                <Th textAlign={"center"}>Name</Th>
-                <Th textAlign={"center"}>Edit Button</Th>
-              </Tr>
-            </Thead>
-            <Tbody textAlign={"center"}>
-              {alldata?.map((item, index) => {
-                return (
-                  <Tr key={index}>
-                    <Td textAlign={"center"}>{item.name}</Td>
-                    <Button onClick={() => HandleEdit(item._id, item.name)}>
-                      Edit
-                    </Button>
+      {alldata.length === 0 ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Spinner alignItems={"center"} />
+        </div>
+      ) : (
+        <>
+          <div>
+            <TableContainer>
+              <Table variant="simple" size="sm">
+                <Thead>
+                  <Tr>
+                    <Th textAlign={"center"}>Name</Th>
+                    <Th textAlign={"center"}>Edit Button</Th>
                   </Tr>
-                );
-              })}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </div>
-      <UpdateModal
-        item={item}
-        isOpen={isOpen}
-        onClose={onClose}
-        onOpen={onOpen}
-        setNewName={setNewName}
-        newname={newname}
-        updateData={handleSave}
-      />
+                </Thead>
+                <Tbody textAlign={"center"}>
+                  {alldata?.map((item, index) => {
+                    return (
+                      <Tr key={index}>
+                        <Td textAlign={"center"}>{item.name}</Td>
+                        <Button
+                          onClick={() => HandleEdit(item._id, item.name)}
+                          m={3}
+                        >
+                          Edit
+                        </Button>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </div>
+          <UpdateModal
+            item={item}
+            isOpen={isOpen}
+            onClose={onClose}
+            onOpen={onOpen}
+            setNewName={setNewName}
+            newname={newname}
+            updateData={handleSave}
+          />
+        </>
+      )}
     </div>
   );
 };
